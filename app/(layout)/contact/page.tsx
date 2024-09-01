dbfaa15af0ec201cc10e87a4854a1d223702fe3b
andy ramaroson <andyramaroson@gmail.com>
2024-09-01 23:29:15 +0200

    [feature](Home)Add toggle button for contact form                                                                                    - Implement useState hook to manage form visibility - Add button to toggle contact form display - Align button to the right

--- app/(layout)/contact/page.tsx
+++ app/(layout)/contact/page.tsx
@@ -1,16 +1,29 @@
+'use client';
+
+import { useState } from "react";
 import SubHero from "@/features/landing/SubHero";
 import CardContact from "@/features/contact/card/CardContact";
 import ContactForm from "@/features/contact/card/ContactForm";
 
-export default function page() {
+export default function Page() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <SubHero className={""} title={"contacter"} subTitle={"Pour me"} />
 
       <CardContact/>
 
-      <ContactForm className="mt-20" />
+      <div className="flex justify-center mt-10">
+        <button 
+          className="px-4 py-2 bg-orange-500 text-white rounded hover:bg-orange-600"
+          onClick={() => setShowForm(!showForm)}
+        >
+          {showForm ? "Masquer le formulaire" : "Formulaire d'inscription"}
+        </button>
+      </div>
 
+      {showForm && <ContactForm className="mt-20" />}
     </>
   );
 }
