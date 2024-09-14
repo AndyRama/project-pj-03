844b14d15c642e976176ecffb0d3777f04b30a22
andy ramaroson <andyramaroson@gmail.com>
2024-09-14 18:35:50 +0200

    (fix) remove feature plan page form

--- app/(account-layout)/account/plan/page.tsx
+++ app/(account-layout)/account/plan/page.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { requiredAuth } from "@/lib/auth/helper";
-// import { PlanForm } from "./PlanForm";
 
-export default async function PlanPage() {
-  const user = await requiredAuth();
-
+const PlanPage: React.FC = () => {
   return (
-    <div className="flex flex-col gap-4 lg:gap-8">
-      <Card>
-        <CardHeader>
-          <CardTitle>Formulaire</CardTitle>
-        </CardHeader>
-        <CardContent>
-          {/* <PlanForm defaultValues={user} /> */}
-        </CardContent>
-      </Card>
-      
+    <div className="container mx-auto p-4">
+      <h1 className="mb-4 text-2xl font-bold">Explore all Plan</h1>
+      <p>Welcome to the Plan page. Here you can begin to explore some ressources.</p>
+      {/* Add more components or functionality for course creation here */}
     </div>
   );
-}
+};
+
+export default PlanPage;
