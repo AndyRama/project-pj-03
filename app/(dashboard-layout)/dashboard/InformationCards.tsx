3c9492208c5e8b068bbd59f612fab555f79cc27e
andy ramaroson <andyramaroson@gmail.com>
2025-02-15 14:34:48 +0100

    refactor: update InformationCards and SubscribersChart with new data and improved labels

--- app/(dashboard-layout)/dashboard/InformationCards.tsx
+++ app/(dashboard-layout)/dashboard/InformationCards.tsx
@@ -11,7 +11,7 @@ export default function InformationCards() {
           <ReceiptEuro  className="size-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
-          <div className="text-2xl font-bold">184</div>
+          <div className="text-2xl font-bold">12</div>
           <p className="text-xs text-muted-foreground">
             +12.5% depuis le mois dernier
           </p>
@@ -23,17 +23,17 @@ export default function InformationCards() {
           <FilePen className="size-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
-          <div className="text-2xl font-bold">+ 128</div>
-          <p className="text-xs text-muted-foreground">+5.2% depuis le mois dernier</p>
+          <div className="text-2xl font-bold">+ 50</div>
+          <p className="text-xs text-muted-foreground">+15.2% depuis le mois dernier</p>
         </CardContent>
       </Card>
       <Card className="w-full flex-1">
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Tendance ventes</CardTitle>
+          <CardTitle className="text-sm font-medium">nombre article</CardTitle>
           <TrendingUp  className="size-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
-          <div className="text-2xl font-bold">+ 46</div>
+          <div className="text-2xl font-bold">+ 8</div>
           <p className="text-xs text-muted-foreground">+25% pour le prochain mois</p>
         </CardContent>
       </Card>
--- app/(dashboard-layout)/dashboard/SubscribersChart.tsx
+++ app/(dashboard-layout)/dashboard/SubscribersChart.tsx
@@ -1,37 +1,17 @@
 "use client";
 
 const SUBSCRIBERS: { date: string; amount: number }[] = [
-  { date: "2023-05-01", amount: 4000 },
-  { date: "2023-05-02", amount: 12050 },
-  { date: "2023-05-03", amount: 12120 },
-  { date: "2023-05-04", amount: 12200 },
-  { date: "2023-05-05", amount: 12300 },
-  { date: "2023-05-06", amount: 12420 },
-  { date: "2023-05-07", amount: 12500 },
-  { date: "2023-05-08", amount: 12630 },
-  { date: "2023-05-09", amount: 12700 },
-  { date: "2023-05-10", amount: 12850 },
-  { date: "2023-05-11", amount: 13000 },
-  { date: "2023-05-12", amount: 13150 },
-  { date: "2023-05-13", amount: 13300 },
-  { date: "2023-05-14", amount: 13450 },
-  { date: "2023-05-15", amount: 13600 },
-  { date: "2023-05-16", amount: 13750 },
-  { date: "2023-05-17", amount: 13920 },
-  { date: "2023-05-18", amount: 14050 },
-  { date: "2023-05-19", amount: 14200 },
-  { date: "2023-05-20", amount: 14350 },
-  { date: "2023-05-21", amount: 14500 },
-  { date: "2023-05-22", amount: 14650 },
-  { date: "2023-05-23", amount: 14800 },
-  { date: "2023-05-24", amount: 14950 },
-  { date: "2023-05-25", amount: 15100 },
-  { date: "2023-05-26", amount: 15250 },
-  { date: "2023-05-27", amount: 15400 },
-  { date: "2023-05-28", amount: 15550 },
-  { date: "2023-05-29", amount: 15700 },
-  { date: "2023-05-30", amount: 15850 },
-  { date: "2023-05-31", amount: 16000 },
+  { date: "2024-09-02", amount: 0 },
+  { date: "2024-10-02", amount: 5 },
+  { date: "2024-11-02", amount: 10 },
+  { date: "2024-12-02", amount: 15 },
+  { date: "2025-01-02", amount: 20 },
+  { date: "2025-02-02", amount: 25 },
+  { date: "2025-03-02", amount: 30 },
+  { date: "2025-04-02", amount: 35 },
+  { date: "2025-05-02", amount: 40 },
+  { date: "2025-06-02", amount: 45 },
+  { date: "2025-07-02", amount: 50 },
 ];
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -69,18 +49,16 @@ export const SubscribersChart = () => {
               type="monotone"
               dataKey="amount"
               stroke="hsl(var(--primary) / 1)"
-              fill={`url(#color-primary)`}
+              fill="url(#color-primary)"
               dot={{
                 stroke: "hsl(var(--primary) / 0.5)",
                 strokeWidth: 1,
                 r: 2,
-                strokeDasharray: "",
               }}
               activeDot={{
                 stroke: "hsl(var(--primary) / 1)",
                 strokeWidth: 1,
                 r: 4,
-                strokeDasharray: "",
               }}
             />
             <Scatter dataKey="amount" fill="hsl(var(--primary))" />
@@ -90,15 +68,16 @@ export const SubscribersChart = () => {
               fontSize={10}
               tickLine={false}
               axisLine={false}
-              interval={5}
+              interval={2} // Affiche une étiquette tous les 2 mois
             />
             <YAxis
+              domain={[0, 100]}
               stroke="hsl(var(--muted-foreground) / 0.5)"
               fontSize={10}
               tickLine={false}
               axisLine={false}
               tickFormatter={(value) => Intl.NumberFormat().format(value)}
-              dataKey={"amount"}
+              dataKey="amount"
             />
             <Tooltip
               cursor={{ stroke: "hsl(var(--primary) / 0.2)", strokeWidth: 2 }}
@@ -106,32 +85,32 @@ export const SubscribersChart = () => {
                 if (active && payload && payload.length > 0) {
                   return (
                     <TooltipChart>
-                      <TooltipChartItem label="Amount">
-                        {Intl.NumberFormat().format(payload[0].payload.value)}
+                      <TooltipChartItem label="Utilisateurs">
+                        {Intl.NumberFormat().format(
+                          payload[0].payload.amount
+                        )}
                       </TooltipChartItem>
-
                       <TooltipChartItem label="Date">
                         {new Date(payload[0].payload.date).toLocaleDateString()}
                       </TooltipChartItem>
                     </TooltipChart>
                   );
                 }
-
                 return null;
               }}
             />
             <defs>
-              <linearGradient id={`color-primary`} x1="0" y1="0" x2="0" y2="1">
+              <linearGradient id="color-primary" x1="0" y1="0" x2="0" y2="1">
                 <stop
                   offset="0%"
                   stopColor="hsl(var(--primary))"
                   stopOpacity={0.4}
-                ></stop>
+                />
                 <stop
                   offset="75%"
                   stopColor="hsl(var(--primary))"
                   stopOpacity={0.05}
-                ></stop>
+                />
               </linearGradient>
             </defs>
           </AreaChart>
