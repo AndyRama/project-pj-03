9e05f32c5544dfded03c8722acc29bf754405fa7
andy ramaroson <andyramaroson@gmail.com>
2025-02-21 10:39:16 +0100

    refactor: rename Page component to PostPage and update heading text in posts page

--- app/(dashboard-layout)/posts/page.tsx
+++ app/(dashboard-layout)/posts/page.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Page: React.FC = () => {
+const PostPage: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="mb-4 text-2xl font-bold">Create </h1>
-      <p>Welcome to the  page create. Here you can begin to create some ressources.</p>
+      <h1 className="mb-4 text-2xl font-bold">Create Post</h1>
+      <p>Welcome to the Post page create. Here you can begin to create some ressources.</p>
       {/* Add more components or functionality for course creation here */}
     </div>
   );
 };
 
-export default Page;
+export default PostPage;
