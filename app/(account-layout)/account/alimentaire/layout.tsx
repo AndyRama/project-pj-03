a511a291802029d912ee96230be6ce5d3eef29ac
andy ramaroson <andyramaroson@gmail.com>
2025-02-24 11:38:05 +0100

    feat: update alimentaire layout with new actions and label changes

--- app/(account-layout)/account/alimentaire/layout.tsx
+++ app/(account-layout)/account/alimentaire/layout.tsx
@@ -1,23 +1,28 @@
-import { SettingsNavigation } from "@/features/layout/SettingsNavigation";
 import {
   Layout,
   LayoutContent,
   LayoutDescription,
   LayoutHeader,
   LayoutTitle,
+  LayoutActions,
 } from "@/features/page/layout";
+import { Button } from "@/components/ui/button";
 import type { LayoutParams } from "@/types/next";
+import { SettingsNavigation } from "@/features/layout/SettingsNavigation";
 
 export default async function RouteLayout(
   props: LayoutParams<{ productId: string }>,
 ) {
   return (
     <Layout>
+      <LayoutTitle>Plan Alimentaire</LayoutTitle>
       <LayoutHeader>
-        <LayoutTitle>Plan Alimentaire</LayoutTitle>
+        <LayoutActions className="hidden gap-6 lg:flex">
+          <Button variant="invert">Booking a visio</Button>
+        </LayoutActions>
         <LayoutDescription>
           Afin que je puisse repondre parfaitement a votre besoins veuillez remplire le formulaire.
-          Je vous enverrez le plan alimentaire par Email et nous pourrons en discuter ensemble lors 
+          Je vous enverrez le plan alimentaire par Email et nous pourrons en discuter ensemble lors
           de notre 1 er call.
         </LayoutDescription>
       </LayoutHeader>
@@ -26,12 +31,12 @@ export default async function RouteLayout(
           links={[
             {
               href: `/settings`,
-              label: "General",
+              label: "Utilisateur",
               badge: ""
             },
             {
               href: `/settings/notifications`,
-              label: "Invoices",
+              label: "information",
               badge: ""
             },
           ]}
