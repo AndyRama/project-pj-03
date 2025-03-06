e06192de473c20b108bfef2e13d1574b50cf5744
andy ramaroson <andyramaroson@gmail.com>
2025-03-06 23:09:38 +0100

    [Landing] fix error footer blur

--- src/features/layout/Footer.tsx
+++ src/features/layout/Footer.tsx
@@ -84,10 +84,10 @@ export const Footer = () => {
               </div>
             ))}
           </div>
+        <div className="transform-gpu blur-3xl">
+          <div className="absolute right-0 -z-20 h-64 w-56 bg-gradient-to-tr from-orange-500 to-orange-800 opacity-30 md:opacity-50"></div>
+        </div>
         </LayoutContent>
-      <div className="transform-gpu blur-3xl">
-        <div className="absolute right-0 -z-20 h-64 w-56 bg-gradient-to-tr from-orange-500 to-orange-800 opacity-30 md:opacity-50"></div>
-      </div>
       </Layout>
     </footer>
   )
