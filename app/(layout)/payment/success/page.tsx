17b0cc46deee43e3a7d5ea23b123bb322eb25fb8
andy ramaroson <andyramaroson@gmail.com>
2025-03-14 18:46:50 +0100

    [Layout] Fix content paiement/succes

--- app/(layout)/payment/success/page.tsx
+++ app/(layout)/payment/success/page.tsx
@@ -7,29 +7,32 @@ import {
   LayoutTitle,
 } from "@/features/page/layout";
 import Link from "next/link";
+import { SectionLayout } from '@/features/landing/SectionLayout';
 
 export default function SuccessPaymentPage() {
   return (
     <>
-      <Layout>
-        <LayoutHeader>
-          <LayoutTitle>Merci pour votre achat !</LayoutTitle>
-          <LayoutDescription>
-            Votre paiement a réussi ! Vous avez désormais un accès complet à tous nos
-            ressources premium. Si vous avez des questions, nous sommes là pour vous aider.
-          </LayoutDescription>
-        </LayoutHeader>
-        <LayoutContent>
-        <LayoutTitle>Completer le formulaire</LayoutTitle>
-          <LayoutDescription>
-            Votre paiement a réussi ! Vous avez désormais un accès complet à tous nos
-            ressources premium. Si vous avez des questions, nous sommes là pour vous aider.
-          </LayoutDescription>
-          <Link href="/" className={buttonVariants({ size: "lg" })}>
-            Accueil
-          </Link>
-        </LayoutContent>
-      </Layout>
+      <SectionLayout size="lg" variant="default" className="mx-auto mt-[-22] max-w-7xl p-6">
+        <Layout>
+          <LayoutHeader>
+            <LayoutTitle>Merci pour votre achat !</LayoutTitle>
+            <LayoutDescription>
+              Votre paiement a réussi ! Vous avez désormais un accès complet à tous nos
+              ressources premium. Si vous avez des questions, nous sommes là pour vous aider.
+            </LayoutDescription>
+          </LayoutHeader>
+          <LayoutContent>
+          <LayoutTitle>Completer le formulaire</LayoutTitle>
+            <LayoutDescription>
+              Votre paiement a réussi ! Vous avez désormais un accès complet à tous nos
+              ressources premium. Si vous avez des questions, nous sommes là pour vous aider.
+            </LayoutDescription>
+            <Link href="/" className={buttonVariants({ size: "md" })}>
+              Accueil
+            </Link>
+          </LayoutContent>
+        </Layout>
+      </SectionLayout>
     </>
   );
 }
