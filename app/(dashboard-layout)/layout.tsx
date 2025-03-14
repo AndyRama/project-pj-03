0d058c91d6f998f3e842efca9d97ce70998abf15
andy ramaroson <andyramaroson@gmail.com>
2025-03-15 00:29:27 +0100

    [Role] Try to fix content dynamic

--- app/(dashboard-layout)/layout.tsx
+++ app/(dashboard-layout)/layout.tsx
@@ -1,6 +1,14 @@
 import type { LayoutParams } from "@/types/next";
 import { DashboardNavigation } from "./DashboardNavigation";
+import { useUserRole } from "@/hooks/useUserRole";
+import { AccountNavigation } from "../(account-layout)/AccountNavigation";
 
-export default async function RouteLayout(props: LayoutParams<{}>) {
-  return <DashboardNavigation>{props.children}</DashboardNavigation>;
+export default function RouteLayout(props: LayoutParams<{}>) {
+  const { isSuperAdmin } = useUserRole();
+
+  if (isSuperAdmin) {
+    return <DashboardNavigation>{props.children}</DashboardNavigation>;
+  } else {
+    return <AccountNavigation>{props.children}</AccountNavigation>;
+  }
 }
