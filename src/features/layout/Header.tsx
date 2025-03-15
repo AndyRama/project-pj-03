ab97c81b5bc86d8f07a8ed56895efe66619e0a71
andy ramaroson <andyramaroson@gmail.com>
2025-03-15 18:11:08 +0100

    [DashBoard] Clean code

--- src/features/layout/Header.tsx
+++ src/features/layout/Header.tsx
@@ -1,11 +1,7 @@
-"use client";
-
 import { HeaderBase } from "./HeaderBase";
-import { SuperAdminHeader } from "./SuperAdminHeader";
-import { useUserRole } from "@/hooks/useUserRole";
 
 export function Header() {
-  const { isSuperAdmin } = useUserRole();
-  
-  return isSuperAdmin ? <SuperAdminHeader /> : <HeaderBase />;
+  return (
+    <HeaderBase/>
+  );
 }
