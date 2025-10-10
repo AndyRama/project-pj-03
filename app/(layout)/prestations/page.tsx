3aaa41bff915be5aff62137cfbfe3e5b28c3392f
andy ramaroson <andyramaroson@gmail.com>
2025-10-10 14:28:31 +0200

    [Fix] error link stripe

--- app/(layout)/prestations/page.tsx
+++ app/(layout)/prestations/page.tsx
@@ -20,7 +20,7 @@ export default function page() {
                 isPopular: true,
                 type: "monthly",
                 id: "",
-                link: "https://buy.stripe.com/test_28E4gB0IM3HB6YaeDR9AA00",
+                link: "https://buy.stripe.com/28E4gB0IM3HB6YaeDR9AA00",
                 title: "Perte de poids",
                 subtitle: "(6 * 160€ / 6 mois)",
                 price: 160,
