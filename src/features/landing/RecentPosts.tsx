731e18db63be5a5a38aabd0e9a5bb68b28917e71
andy ramaroson <andyramaroson@gmail.com>
2026-01-05 18:34:33 +0100

    [Fix] adjust layout and spacing in Hero and RecentPosts components for improved presentation

--- src/features/landing/RecentPosts.tsx
+++ src/features/landing/RecentPosts.tsx
@@ -105,24 +105,24 @@ export const RecentPosts = () => {
   return (
     <Layout>
       <div className="mx-auto -mt-32 mb-2 justify-center rounded-r-md md:flex md:px-4">
-        <div
-          className="mx-auto mt-10 grid w-full grid-cols-1 gap-6 text-gray-500 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
-        >
-          {posts.map((post, index) => (
-            <RecentPostCard key={index} post={post} />
-          ))}
-        </div>
-        
-        <div className="mt-12 flex justify-center">
-          <Link
-            href="/blog"
-            className={cn(
-              buttonVariants({ size: "lg", variant: "default" }),
-              "text-base font-bold text-white border shadow-md transition-all duration-300 hover:scale-105 hover:bg-orange-800 hover:shadow-lg bg-orange-700",
-            )}
-          >
-            Voir tous les articles
-          </Link>
+        <div className="relative mx-auto mt-10 w-full">
+          <div className="grid w-full grid-cols-1 gap-6 text-gray-500 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {posts.map((post, index) => (
+              <RecentPostCard key={index} post={post} />
+            ))}
+          </div>
+          
+          <div className="mt-8 flex justify-end">
+            <Link
+              href="/blog"
+              className={cn(
+                buttonVariants({ size: "lg", variant: "default" }),
+                "text-base font-bold text-white shadow-md transition-all duration-300 hover:scale-105 hover:shadow-lg bg-orange-600 hover:bg-orange-700",
+              )}
+            >
+              Voir tous les articles
+            </Link>
+          </div>
         </div>
       </div>
     </Layout>
